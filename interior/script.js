83ef25279082356e1b6aad79c3adecf00c7c186e
unknown <rezaulkarimpstu@gmail.com>
2023-07-16 08:59:08 +0600

    Created Hamberger Menu using javaScript-Done

--- interior/script.js
+++ interior/script.js
@@ -1,1 +1,23 @@
 "use strict";
+// finding element
+const openBtn = document.querySelector(".open");
+const closeBtn = document.querySelector(".close");
+const hamClose = document.querySelector(".hamberger-close");
+const navMenu = document.querySelector(".nav-list");
+const header = document.querySelector(".header");
+
+
+
+
+openBtn.addEventListener("click",function(){
+navMenu.classList.add("open-menu");
+header.classList.add("open-menu");
+hamClose.classList.add("open-menu");
+
+})
+closeBtn.addEventListener("click",function(){
+navMenu.classList.remove("open-menu");
+header.classList.remove("open-menu");
+hamClose.classList.remove("open-menu");
+
+})
